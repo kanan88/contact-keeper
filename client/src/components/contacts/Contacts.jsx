d0ca7a981b4371ce9c1ba6bd2e4c9f760432c9eb
Kanan Farzaliyev <kananfarzaliyev@Kanans-iMac.local>
2020-05-15 23:18:50 +0100

    client side setup & contacts ui

---
+++ client/src/components/contacts/Contacts.jsx
@@ -0,0 +1,30 @@
+import React, {useContext} from 'react';
+import ContactContext from '../../context/contact/contactContext';
+import ContactItem from "./ContactItem";
+
+const Contacts = () => {
+    const contactContext = useContext(ContactContext);
+
+    const {contacts, filtered} = contactContext;
+
+    if (contacts.length === 0) {
+        return <h4>Please add a contact</h4>
+    }
+
+    return (
+        <>
+            {filtered !== null
+                ?
+                filtered.map(contact => (
+                    <ContactItem  contact={contact}/>
+                ))
+                :
+                contacts.map(contact => (
+                    <ContactItem  key={contact.id} contact={contact}/>
+                ))
+            }
+        </>
+    );
+}
+
+export default Contacts;
--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import Contacts from "../contacts/Contacts";
+import ContactForm from "../contacts/ContactForm";
+import ContactFilter from "../contacts/ContactFilter";
 
-Home.propTypes = {
-    
-};
-
-function Home(props) {
+const Home = () => {
     return (
-        <div>Home</div>
+        <div className="grid-2">
+            <div>
+                <ContactForm />
+            </div>
+            <div>
+                <ContactFilter />
+                <Contacts />
+            </div>
+        </div>
     );
 }
 
